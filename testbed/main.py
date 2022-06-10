aac66ff20a1f0f3b2b4998e3372da305ffba73dd
Tyler <tlabonte@gatech.edu>
2022-06-10 14:02:23 -0400

    imports

--- testbed/main.py
+++ testbed/main.py
@@ -4,8 +4,8 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, TQDMProgressBar
 from pytorch_lightning.utilities.seed import seed_everything
 
-from .args import parse_args
-from .resnet import ResNet
+from args import parse_args
+from resnet import ResNet
 
 
 def load_model(args, model_class, classes):
