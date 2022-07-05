86861ea8824fee7832a3d989390307a84251f809
Tyler LaBonte <tlabonte@gatech.edu>
2022-07-05 20:10:24 +0000

    updates

--- exps/eigenmax.py
+++ exps/eigenmax.py
@@ -1,9 +1,9 @@
 import os.path as osp
 
 import torch
-from torch.autograd.functional import hessian
-from torch.nn.utils import _stateless
+import torch.nn.functional as F
 
+from hessian_eigenthings import compute_hessian_eigenthings
 import matplotlib.pyplot as plt
 
 from testbed.args import parse_args
@@ -15,25 +15,11 @@ class ResNetExp(ResNet):
         super().__init__(args, classes)
 
     def validation_epoch_end(self, validation_step_outputs):
-        imgs = torch.stack([result["imgs"] for result in validation_step_outputs])
-        targets = torch.stack([result["targets"] for result in validation_step_outputs])
-        batch = (imgs, targets)
-
-        names = list(n for n, _ in self.model.named_parameters())
-        def loss(*params):
-            result: torch.Tensor = _stateless.functional_call(self.step, {n: p for n, p in zip(names, params)}, batch, 0)
-            return result["loss"]
-
-        if self.current_epoch == 1:
-            print(hessian(loss, tuple(self.model.parameters())))
-
-            """
-            plt.hist([param.weight for param in self.model.parameters()], 50)
-            plt.xlabel("Weight")
-            plt.ylabel("Frequency")
-            plt.title(f"ResNet CIFAR-10 Epoch {self.current_epoch} Weight Distribution")
-            plt.savefig(osp.join(self.hparams.out_dir, f"weights{self.current_epoch}.png")
-            """
+        if self.current_epoch == 0:
+            torch.set_grad_enabled(True)
+            loss = F.cross_entropy
+            loader = self.trainer.val_dataloaders[0]
+            print(compute_hessian_eigenthings(self.model, loader, loss, 1)[0])
 
 if __name__ == "__main__":
     args = parse_args()
