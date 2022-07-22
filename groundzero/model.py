b7c0aafa6360f0ddd37784c43163bfe2b64340c1
Tyler LaBonte <tlabonte@gatech.edu>
2022-07-22 10:47:22 -0700

    Update model.py

--- groundzero/model.py
+++ groundzero/model.py
@@ -43,16 +43,8 @@ class Model(pl.LightningModule):
             gamma=self.hparams.lr_drop,
         )
 
-        cfg = {
-            "optimizer": optimizer,
-            "lr_scheduler": {
-                "scheduler": scheduler,
-                "monitor": "val_loss",
-            },
-        }
-
-        return cfg
-
+        return [optimizer], [scheduler]
+    
     def step(self, batch, idx):
         inputs, targets = batch
 
@@ -71,7 +63,7 @@ class Model(pl.LightningModule):
 
         acc1, acc5 = compute_accuracy(result["probs"], result["targets"])
         result["acc1"] = acc1
-        self.log("loss", result["loss"], on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("train_loss", result["loss"], on_step=True, on_epoch=True, prog_bar=True, sync_dist=True)
         self.log("train_acc1", acc1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
         self.log("train_acc5", acc5, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
