de6f0f75e9b50bbf4989baa4e96cdeeed7e781ea
Tyler LaBonte <tlabonte@gatech.edu>
2022-07-22 10:30:17 -0700

    Update model.py

--- groundzero/model.py
+++ groundzero/model.py
@@ -70,8 +70,8 @@ class Model(pl.LightningModule):
         result = self.step(batch, idx)
 
         acc1, acc5 = compute_accuracy(result["probs"], result["targets"])
-        #self.log("train_loss", result["loss"], on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
         result["acc1"] = acc1
+        self.log("loss", result["loss"], on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
         self.log("train_acc1", acc1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
         self.log("train_acc5", acc5, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
