06eaa27064be05b26372b3f231b0197d908f8f00
Tyler LaBonte <tlabonte@gatech.edu>
2022-07-25 09:16:25 -0700

    Update measures.py

--- exps/measures.py
+++ exps/measures.py
@@ -3,6 +3,7 @@ from math import log
 import os.path as osp
 
 import torch
+import torch.nn.functional as F
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,8 +14,11 @@ from groundzero.main import main
 from groundzero.mlp import MLP
 from groundzero.utils import compute_accuracy
 
+SIGMA = [0.1]
 TRAIN_ACC = []
 TEST_ACC = []
+SHARP = []
+SHARP_APX = []
 PROD_SPEC = []
 PROD_FRO = []
 MARGIN = []
@@ -33,6 +37,23 @@ class MeasuresMLP(MLP):
         result = super().training_step(batch, idx)
 
         with torch.no_grad():
+            # Sharpness 2-layer only
+            inputs, targets = batch
+            w1 = deepcopy(self.model[0].weight)
+            for sigma in SIGMA:
+                self.model[0].weight = torch.normal(w1, sigma)
+                logits = self.model(inputs)
+                result["sharp"] = F.cross_entropy(logits, targets)
+                
+                wx = w1 @ x
+                sigmoid_apx = (1 + torch.exp(-wx)) ** -1
+                sigmoid_apx += (784 * sigma ** 2) / 2 * ((2 * torch.exp(-2 * wx) / (1 + torch.exp(-wx)) ** 3) - (torch.exp(-wx) / (1 + torch.exp(-wx)) ** 2))
+                logits_apx = self.model[self.fc_layers[1]].weight @ sigmoid_apx
+                result["sharp_apx"] = F.cross_entropy(logits_apx, targets)
+                
+            self.model[0].weight = w1
+            
+            # Margin
             probs_wo_true = deepcopy(result["probs"])
             inds = torch.repeat_interleave(result["targets"].unsqueeze(1), self.hparams.classes, dim=1)
             probs_wo_true.scatter_(1, inds, 0)
@@ -45,17 +66,22 @@ class MeasuresMLP(MLP):
         
         TRAIN_ACC.append(self.train_acc1)
         
+        sharps = to_np(torch.cat([result["sharp"] for result in training_step_outputs]))
+        sharps_apx = to_np(torch.cat([result["sharp_apx"] for result in training_step_outputs]))
+        SHARP.append(sharps.mean())
+        SHARP_APX.append(sharps_apx.mean())
+        
         margins = to_np(torch.cat([result["margin"] for result in training_step_outputs]))
         MARGIN.append(np.percentile(margins, 10))
+        
+        weights = [self.model[i].weight for i in self.fc_layers]
+        PROD_SPEC.append(to_np(torch.stack([torch.linalg.norm(w, ord=2) for w in weights])).prod())
+        PROD_FRO.append(to_np(torch.stack([torch.linalg.norm(w, ord="fro") for w in weights])).prod())
 
     def validation_epoch_end(self, validation_step_outputs):
         super().validation_epoch_end(validation_step_outputs)
         
         TEST_ACC.append(self.val_acc1)
-                      
-        weights = [self.model[i].weight for i in self.fc_layers]
-        PROD_SPEC.append(to_np(torch.stack([torch.linalg.norm(w, ord=2) for w in weights])).prod())
-        PROD_FRO.append(to_np(torch.stack([torch.linalg.norm(w, ord="fro") for w in weights])).prod())
         
 def experiment(args):
     global TRAIN_ACC, TEST_ACC, PROD_SPEC, PROD_FRO, MARGIN
@@ -72,6 +98,8 @@ def experiment(args):
     
     TRAIN_ACC = np.asarray(TRAIN_ACC)
     TEST_ACC = np.asarray(TEST_ACC)
+    SHARP = np.asarray(SHARP)
+    SHARP_APX = np.asarray(SHARP_APX)
     PROD_SPEC = np.asarray(PROD_SPEC)
     PROD_FRO = np.asarray(PROD_FRO)
     MARGIN = np.asarray(MARGIN)
@@ -85,7 +113,16 @@ def experiment(args):
         plt.title("MNIST, SGD 0.05, WD 0, B 256, Loss 0.01")
         plt.savefig(osp.join(args.out_dir, f"{name}.png"))
         plt.clf()
-
+        
+    x = np.arange(len(SHARP))
+    plt.plot(x, SHARP, label="Actual", linestyle="dashed")
+    plt.plot(x, SHARP_APX, label="Approximation", linestyle="solid")
+    plt.xlabel("Epoch")
+    plt.ylabel("Sharpness")
+    plt.title("MNIST, SGD 0.05, WD 0, B 256, Loss 0.1")
+    plt.savefig(osp.join(args.out_dir, "sharpness.png"))
+    plt.clf()
+             
     plot(PROD_SPEC, "Product of Spectral Norms", "prod_spec")
     plot(PROD_FRO, "Product of Frobenius Norms", "prod_fro")
     
