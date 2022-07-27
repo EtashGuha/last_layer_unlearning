77f6f096e7c9233ab3a2698f345937d2d447a747
Tyler LaBonte <tlabonte@gatech.edu>
2022-07-27 10:10:19 -0700

    Update sharpness_colab.py

--- exps/sharpness_colab.py
+++ exps/sharpness_colab.py
@@ -117,7 +117,7 @@ def experiment(args):
     SHARP_APX3 = np.asarray(SHARP_APX3)
 
     with open(osp.join(args.out_dir, "sharp.pkl"), "wb") as f:
-        pickle.dump(f, np.concatenate((SHARP, SHARP_APX1, SHARP_APX2, SHARP_APX3), axis=0))
+        pickle.dump(np.concatenate((SHARP, SHARP_APX1, SHARP_APX2, SHARP_APX3), axis=0), f)
     
     for j, sigma in enumerate(SIGMA):
         sharp = moving_average(SHARP[:, j], MOVING_AVG)
