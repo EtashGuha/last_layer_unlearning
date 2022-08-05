b911d8ce9a1c0146c82581553ce97ffd0e69383d
Tyler LaBonte <tlabonte@gatech.edu>
2022-08-05 16:57:30 -0700

    Update cnn.py

--- groundzero/cnn.py
+++ groundzero/cnn.py
@@ -12,12 +12,14 @@ class CNN(Model):
         h = [args.cnn_initial_width * j for j in [2 ** i for i in range(0, args.cnn_num_layers)]]
         
         channels = zip([args.cnn_input_dim] + h[:-1], h)
-        for n, k in channels:
-            self.model.append(nn.Conv2d(n, k, args.cnn_kernel_size, bias=args.bias))
+        for j, (n, k) in channels:
+            self.model.append(nn.Conv2d(n, k, args.cnn_kernel_size, bias=args.bias, padding=args.cnn_padding))
             self.model.append(nn.BatchNorm2d(k))
             self.model.append(nn.ReLU(inplace=True))
-            self.model.append(nn.MaxPool2d(2))
+            if j != 0:
+                self.model.append(nn.MaxPool2d(2))
         
+        self.model.append(nn.MaxPool2d(4))
         self.model.append(nn.Flatten())
         self.model.append(nn.LazyLinear(args.classes, bias=args.bias))
 
