58d8584f780aeb215ac8d2fed5da1d59ad4b2df1
Tyler LaBonte <tlabonte@gatech.edu>
2022-08-05 21:29:46 -0700

    Update cnn.py

--- groundzero/cnn.py
+++ groundzero/cnn.py
@@ -6,9 +6,9 @@ from groundzero.model import Model
 class CNN(Model):
     def __init__(self, args, classes):
         super().__init__(args, classes)
-
+        
         self.model = nn.Sequential()
-
+        
         h = [args.cnn_initial_width * j for j in [2 ** i for i in range(0, args.cnn_num_layers)]]
         
         channels = zip([args.cnn_input_dim] + h[:-1], h)
@@ -21,15 +21,17 @@ class CNN(Model):
         
         self.model.append(nn.MaxPool2d(4))
         self.model.append(nn.Flatten())
-        self.model.append(nn.LazyLinear(args.classes, bias=args.bias))
-
+        self.model.append(nn.LazyLinear(256, bias=args.bias))
+        self.model.append(nn.ReLU(inplace=True))
+        self.model.append(nn.Linear(256, args.classes, bias=args.bias))
+        
         if args.train_fc_only:
             for p in self.model.parameters():
                 p.requires_grad = False
-            for p in self.model[-1].parameters():
+            for p in self.model[-2:].parameters():
                 p.requires_grad = True
 
     def forward(self, inputs):
         outputs = self.model(inputs)
-
+        
         return outputs
