f27d764650e42f6f6d34362984d6716be60ab49e
Tyler LaBonte <tlabonte@gatech.edu>
2022-08-09 14:14:21 -0700

    Update dataset.py

--- groundzero/datasets/dataset.py
+++ groundzero/datasets/dataset.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 import random
 
+from torch import randperm
+
 from pl_bolts.datamodules.vision_datamodule import VisionDataModule
 
 
@@ -25,6 +27,10 @@ class Dataset(VisionDataModule):
 
         self.data_augmentation = args.data_augmentation
         self.label_noise = args.label_noise
+        
+        self.train_transforms = None
+        self.val_transforms = None
+        self.test_transforms = None
 
         if self.data_augmentation:
             self.train_transforms = self.augmented_transforms()
@@ -59,17 +65,19 @@ class Dataset(VisionDataModule):
             dataset_train = self.dataset_class(self.data_dir, train=True, transform=train_transforms)
             dataset_val = self.dataset_class(self.data_dir, train=True, transform=val_transforms)
 
-            self.dataset_train = self._split_dataset(dataset_train)
-            self.dataset_val = self._split_dataset(dataset_val, train=False)
-
+            gen = torch.Generator().manual_seed(self.seed)
+            train_indices = randperm(sum(lengths), generator=gen).tolist()[:self._get_splits[0]]
             if self.label_noise:
-                num_labels = len(self.dataset_train)
+                num_labels = len(train_indices)
                 num_noised_labels = int(self.label_noise * num_labels)
 
                 # Support labels not called "targets"?
-                for i, target in enumerate(self.dataset_train.targets[:num_noised_labels]):
+                for i, target in enumerate(train_indices[:num_noised_labels]):
                     labels = [j for j in range(num_classes) if j != i]
-                    self.dataset_train.targets[i] = random.choice(labels)
+                    dataset_train.targets[i] = random.choice(labels)
+            
+            self.dataset_train = self._split_dataset(dataset_train)
+            self.dataset_val = self._split_dataset(dataset_val, train=False)
 
         if stage == "test" or stage is None:
             test_transforms = self.default_transforms() if self.test_transforms is None else self.test_transforms
