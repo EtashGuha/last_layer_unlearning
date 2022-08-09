90cdae2e75b2dc7697f68b89d774ea5d32579bb9
Tyler LaBonte <tlabonte@gatech.edu>
2022-08-09 15:04:54 -0700

    Update overfit.py

--- exps/overfit.py
+++ exps/overfit.py
@@ -1,4 +1,9 @@
+from math import prod
 import os.path as osp
+
+from torch.fft import fft2
+from torch.linalg import svdvals
+from torch.nn import Conv2D
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,26 +13,39 @@ from groundzero.datasets.cifar10 import CIFAR10
 from groundzero.main import main
 from groundzero.models.cnn import CNN
 
-DEPTHS = [2, 4]
-WIDTHS = [32, 64, 128, 256]
+WIDTHS = [2, 4, 6, 8, 10]
 
 
+class OverfitCNN(CNN):
+    def __init__(self, args):
+        super().__init__(args)
+        
+    def test_step(self, batch, batch_idx):
+        result = super().test_step(batch, batch_idx)
+        
+        top_svs = []
+        for layer in self.model:
+            if isinstance(layer, Conv2D):
+                transforms = fft2(Conv2D.weights)
+                top_svs.append(svdvals(transforms)[0])
+        
+        result["test_prod_spec"] = prod(top_svs)
+        self.log("test_prod_spec", result["test_prod_spec"], on_epoch=True, prog_bar=False, sync_dist=True)
+        
+        return result
+            
 def experiment(args):
     accs = []
-    for depth in DEPTHS:
-        a = []
-        args.cnn_num_layers = depth
-        for width in WIDTHS:
-            args.cnn_initial_width = width
-            result = main(args, CNN, CIFAR10)
-            a.append(result[0]["test_acc1"])
-        accs.append(a)
+    norms = []
+    for width in WIDTHS:
+        args.cnn_initial_width = width
+        result = main(args, OverfitCNN, CIFAR10)
+        accs.append(result[0]["test_acc1"])
+        norms.append(result[0]["test_prod_spec"])
 
     accs = 1 - np.asarray(accs)
-    plt.plot(WIDTHS, accs[0], label="2 layer CNN")
-    plt.plot(WIDTHS, accs[1], label="4 layer CNN")
+    plt.plot(WIDTHS, accs)
     plt.xlabel("CNN Width Parameter")
-    plt.xscale("log", base=2) 
     plt.ylabel("Test Error")
     plt.legend()
     plt.title(f"CIFAR-10, {args.optimizer} {args.lr}, B {args.batch_size}, {args.max_epochs} epochs")
